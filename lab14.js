162bb0062fa545432f0640731c354c0e9b3d4a5b
67070123 <67070123@kmitl.ac.th>
2024-10-11 12:40:42 +0700

    commit2

---
+++ lab14.js
@@ -0,0 +1,55 @@
+
+let phone_list = []
+
+document.getElementById("displayusername").innerHTML = "My Username's Notebook";
+
+function set_usrname(){
+    let x = document.getElementById("usrname").value;
+    document.getElementById("displayusername").innerHTML = "My " + x + "'s Notebook";
+}
+
+function set_avatarimg(){
+    let i = document.getElementById("pfpurl").value;
+    document.getElementById("pfp").src = i;
+}
+
+
+function add_phone(){
+    let name = document.getElementById("nametext").value;
+    let tel = document.getElementById("teltext").value;
+    phone_list.push([name, tel])
+    display_phone_list()
+}
+
+function display_phone_list(){
+    let table = document.getElementById("phonetable");
+    while (table.firstChild){
+        table.removeChild(table.lastChild)
+    }
+    let headd = document.createElement("th");
+    headd.innerHTML = "No."
+    table.appendChild(headd)
+    let headdn = document.createElement("th");
+    headdn.innerHTML = "Name"
+    table.appendChild(headdn)
+    let headdt = document.createElement("th");
+    headdt.innerHTML = "Tel"
+    table.appendChild(headdt)
+    for (i = 0; i < phone_list.length; i++){
+        let trow = document.createElement("tr");
+        trow.setAttribute("class", "plrow");
+        table.appendChild(trow);
+        let no = document.createElement("td");
+        no.innerHTML = i + 1;
+        let namea = document.createElement("td")
+        namea.innerHTML = phone_list[i][0];
+        let tel = document.createElement("td")
+        tel.innerHTML = phone_list[i][1];
+        trow.appendChild(no)
+        trow.appendChild(namea)
+        trow.appendChild(tel)
+    }
+    
+}
+
+display_phone_list()
